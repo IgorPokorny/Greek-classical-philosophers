0a057d8cdf64c02e4203b3ce12631d7251aa1565
Igor Pokorny <igor.pokorny.1@gmail.com>
2017-02-26 19:43:49 +0100

    Add JQuery file with "hide().fadeIn" & "animate" functions

---
+++ GRphilosophy.js
@@ -0,0 +1,20 @@
+
+
+
+$(document).ready(function(){
+	
+	$("h1").hide().fadeIn(2000);
+	$("h2").hide().fadeIn(4000);
+	$("a").hide().fadeIn(3000);
+	$(".philosopher-img").hide().fadeIn(1200);
+
+	$(".motto").animate({right: '250px'},2000);
+
+
+     
+// $(document).ready(function(){
+            
+//     $("h1").fadeIn(3000);
+                
+});
+       
