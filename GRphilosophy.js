65aefb8467ad6ca8a08c66270f5acf494ee297d9
Igor Pokorny <igor.pokorny.1@gmail.com>
2017-03-01 13:01:11 +0100

    Add footer into HTML- "based on Wikipedia" + add "figcaption" hide&fadeIn into JS

--- GRphilosophy.js
+++ GRphilosophy.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
 	$("h2").hide().fadeIn(4000);
 	$("a").hide().fadeIn(3000);
 	$(".philosopher-img").hide().fadeIn(1200);
+	$("figcaption").hide().fadeIn(3000);
 
 	$(".motto").css({right: '-750px'}).animate({right: '0px'},2000);
 
