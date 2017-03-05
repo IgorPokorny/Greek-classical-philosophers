cbf07a30ac02e77f8814cbfee23ddd9d4ac06c0e
Igor Pokorny <igor.pokorny.1@gmail.com>
2017-03-05 14:32:08 +0100

    Fixed Lightbox

--- GRphilosophy.js
+++ GRphilosophy.js
@@ -3,13 +3,13 @@
 
 $(document).ready(function(){
 	
-	$("h1").hide().fadeIn(2000);
+	$("h1").hide().fadeIn(3000);
 	$("h2").hide().fadeIn(4000);
 	$("a").hide().fadeIn(3000);
-	$(".philosopher-img").hide().fadeIn(1200);
+	$(".philosopher-img").hide().fadeIn(3000);
 	$("figcaption").hide().fadeIn(3000);
 
-	$(".motto").css({right: '-750px'}).animate({right: '0px'},2000);
+	$(".motto").css({right: '-900px'}).animate({right: '0px'},2000);
 
 
      
